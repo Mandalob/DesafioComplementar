d87256e47c45fa3c8f69342cb5771cd046adc2e3
Mandalob <109694694+Mandalob@users.noreply.github.com>
2024-06-18 16:43:58 -0300

    Add files via upload

---
+++ desafio.js
@@ -0,0 +1,16 @@
+let nome= prompt("Insira seu nome");
+let saida = 'Olá,' + " " + nome
+alert(saida)
+
+const n1= prompt("Insira o primeiro número");
+const n2= prompt("Insira o segundo núumero");
+
+const soma= parseInt(n1) + parseInt(n2)
+console.log(soma)
+
+const texto1= prompt("Comida favorita")
+const texto2= prompt("Comida que não gosta")
+
+console.log(`Gosto muito de ${texto1}, mas não gosto de ${texto2}`)
+
+alert(`Gosto muito de ${texto1}, mas não gosto de ${texto2}`)
